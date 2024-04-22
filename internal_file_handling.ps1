d77f4936077a8c3167e598bd9c0c674305d94a73
Turra <turra.m@husky.neu.edu>
2024-04-22 18:30:52 -0400

    Built workflow that migrates old files to respective archived folder after processing job

--- internal_file_handling.ps1
+++ internal_file_handling.ps1
@@ -1,7 +1,11 @@
 # Set file directory 
 $dataDir =      '\\network\Team\MT\Company\CampaignMain'
 $scriptDir = 	'\\network\Team\MT\Company\CampaignMain\Scripts'
-$archiveDir =  '\\network\Team\MT\Company\CampaignMain\Archive'
+
+# Set file destination folders  
+$outreach1Dir = 	'\\network\Team\MT\Company\CampaignMain\Campaign1'  
+$outreach2Dir = 	'\\network\Team\MT\Company\CampaignMain\Campaign1'
+$outreach3Dir = 	'\\network\Team\MT\Company\CampaignMain\Campaign3'
 
 # Pull all file names for files being dropped in file directory
 $files = Get-ChildItem $dataDir -Filter *.txt
@@ -18,14 +22,35 @@ foreach ($file in $files){
 		# Check if name matches
 		if ($filename -match '^(Campaign)_(\d+)$'){
 			$newFilename = $matches[1] + "_orders"
+
+			# Create new filename with .csv extension
+			$newFilename += ".csv"
+			$newFilePath = Join-Path -Path (Get-Location) -ChildPath $newFilename
+			
+			# Establish oldFilePath to original file
+			$oldFilePath = Join-Path -Path $outreach1Dir -ChildPath $file.name
 		}
 		
 		elseif ($filename -match '^(Campaign_projections)_(\d+)$'){
 			$newFileName = $matches[1]
+
+						# Create new filename with .csv extension
+			$newFilename += ".csv"
+			$newFilePath = Join-Path -Path (Get-Location) -ChildPath $newFilename
+			
+			# Establish oldFilePath to original file
+			$oldFilePath = Join-Path -Path $outreach2Dir -ChildPath $file.name
 		}
 		
 		elseif ($filename -match '^(Campaign_output)_(\d+)$'){
 			$newFilename = "Campaign_outreaches"
+			
+			# Create new filename with .csv extension
+			$newFilename += ".csv"
+			$newFilePath = Join-Path -Path (Get-Location) -ChildPath $newFilename
+			
+			# Establish oldFilePath to original file
+			$oldFilePath = Join-Path -Path $outreach3Dir -ChildPath $file.name
 		}
 		
 		else {
@@ -33,20 +58,13 @@ foreach ($file in $files){
 			continue # Skip to next file if file match not found
 		}
 			
-		# Create new filename with .csv extension
-		$newFilename += ".csv"
-		
-		# Create new path for $newFilename
-		$newFilePath = Join-Path -Path $dataDir -ChildPath $newFilename
-		
 		# Copy original file to $newFileName and path
 		Copy-Item -Path $file.FullName -Destination $newFilePath
 		
 		# Move .txt file to archived folder
-		Move-Item -Path $file -Destination $archiveDir
+		Move-Item -Path $file.FullName -Destination $oldFilePath
 		
 		# Output name of new file
 		Write-Host "New file was created:" $newFilename -ForegroundColor Green
-		# Write-Host $newFIlename
 	}
 }
