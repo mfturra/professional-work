4ed731d9e1c2f80fb0b44fdda6861233cb050a11
mfturra <matheus.turra.b@gmail.com>
2024-05-06 14:44:31 -0400

    Workflow finalized that ingests an excel file into a local database's table

--- app/app.js
+++ app/app.js
@@ -26,16 +26,36 @@ async function importExcelData(filename, sheetname) {
 
     try {
         await client.query('BEGIN');
+        let rowCount = 0;
         worksheet.eachRow({ includeEmpty: false }, async (row, rowNumber) => {
-            if (rowNumber > 14) { // skip header roww
-                const values = row.values.slice(1); // skip first column assuming it contains an ID
+            if (rowNumber >= 2) { // skip header row
+                const values = [
+                    row.getCell(2).value, // Jan
+                    row.getCell(3).value, // Feb
+                    row.getCell(4).value, // Mar
+                    row.getCell(5).value, // Apr
+                    row.getCell(6).value, // May
+                    row.getCell(7).value, // Jun
+                    row.getCell(8).value, // Jul
+                    row.getCell(9).value, // Aug
+                    row.getCell(10).value, // Sep
+                    row.getCell(11).value, // Oct
+                    row.getCell(12).value, // Nov
+                    row.getCell(13).value, // Dec
+                    row.getCell(1).value // year
+                ];
                 const query = {
-                    text: `INSERT INTO practice_table (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)
-                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+                    text: `INSERT INTO practice_table (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec, year)
+                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`,
                     values: values,
                 };
                 await client.query(query);
-            }
+            };
+            // rowCount++;
+
+            // if (rowCount >= 22) {   // Stop after 9th row
+            //     return false;       // Stop iterating through rows
+            // }
         });
         await client.query('COMMIT');
         console.log('Data imported successfully.');
@@ -90,6 +110,20 @@ async function createTable(tableName, columns) {
     }
 }
 
+// Update table by adding a column
+async function addColumnToTable(columnName, columnType) {
+    const client = await pool.connect();
+    try {
+        const queryString = `ALTER TABLE practice_table ADD COLUMN "${columnName}" "${columnType}"`;
+        await client.query(queryString);
+        console.log('Column "${columnName}" was added successfully.');
+    } catch (error) {
+        console.error('Error adding column to table:', error);
+    } finally {
+        client.release();
+    }
+}
+
 const columns = [
     { name: 'jan', type: 'INTEGER' },
     { name: 'feb', type: 'INTEGER' },
@@ -107,6 +141,7 @@ const columns = [
 
 // Utilization of functions
 // createTable('practice_table', columns);
+// addColumnToTable('year', 'NUMERIC')
 
 importExcelData('SeriesReport.xlsx', 'BLS Data Series');
 // setupDatabase('practice_db_connection_test');
