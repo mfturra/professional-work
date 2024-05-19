651ebe96b3c499d499865ecad7de75d2a89486a7
mfturra <matheus.turra.b@gmail.com>
2024-05-18 20:35:41 -0400

    PostgreSQL continual learnings were documented to be used in the future as a point of reference

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # api-data-processing
 Code base for professional projects that involve API requests and data processing of large scale data sets
+
+## PostgreSQL Login
+psql postgres
+
+### Pipeline creation
+Order of workflows that were created.
+1. Create new database for specific user credentials.
+2. Delete specific database using database name.
+3. Create structure table inside database based on specific column types.
+4. Ingest excel data into databases' table.
+5. Upload data onto HTML page and customize table to make it easily understood.
+6. Create login page that requires credentials from user before accessing data.
+
+
+Postgresql DB Tricks
+- Move into database: \connect dbname
+- Output all table names: \dt
+- Output all content inside table: SELECT * FROM tableName;
+
+create postgresql user specifically to handle datasets.
+learn about postgresql pools 
--- app/app.js
+++ app/app.js
@@ -5,10 +5,9 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 
 // database req libraries
-// const { createServer } = require('http');
 const { Pool } = require('pg');
-const exceljs = require('exceljs');
-const fs = require('fs')
+// const exceljs = require('exceljs');
+// const fs = require('fs')
 require('dotenv').config();
 
 const app = express();
@@ -115,135 +114,7 @@ app.listen(port, () => {
 // const html = htmlTemplate.replace('<!-- Table data to be injected here -->', tableRows);
 
 
-// Read Excel data and insert into database
-async function importExcelData(filename, sheetname) {
-    const workbook = new exceljs.Workbook();
-    await workbook.xlsx.readFile(filename);
 
-    const worksheet = workbook.getWorksheet(sheetname);
-    const client = await pool.connect();
-
-    try {
-        await client.query('BEGIN');
-        let rowCount = 0;
-        worksheet.eachRow({ includeEmpty: false }, async (row, rowNumber) => {
-            if (rowNumber >= 2) { // skip header row
-                const values = [
-                    row.getCell(2).value, // Jan
-                    row.getCell(3).value, // Feb
-                    row.getCell(4).value, // Mar
-                    row.getCell(5).value, // Apr
-                    row.getCell(6).value, // May
-                    row.getCell(7).value, // Jun
-                    row.getCell(8).value, // Jul
-                    row.getCell(9).value, // Aug
-                    row.getCell(10).value, // Sep
-                    row.getCell(11).value, // Oct
-                    row.getCell(12).value, // Nov
-                    row.getCell(13).value, // Dec
-                    row.getCell(1).value // year
-                ];
-                const query = {
-                    text: `INSERT INTO practice_table (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec, year)
-                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`,
-                    values: values,
-                };
-                await client.query(query);
-            };
-            // rowCount++;
-
-            // if (rowCount >= 22) {   // Stop after 9th row
-            //     return false;       // Stop iterating through rows
-            // }
-        });
-        await client.query('COMMIT');
-        console.log('Data imported successfully.');
-    } catch (error) {
-        await client.query('ROLLBACK');
-        console.error('Error importing data:', error)
-    } finally {
-        client.release();
-    }
-
-}
-
-// Create a new database
-async function setupDatabase(databaseName) {
-    const client = await pool.connect();
-    try {
-        await client.query(`CREATE DATABASE ${databaseName}`);
-        console.log(`Database "${databaseName}" created successfully.`);
-    } catch (error) {
-        console.error('Error creating database:', error);
-    } finally {
-        client.release();
-    }
-}
-
-// Drop a database
-async function dropDatabase(databaseName) {
-    const client = await pool.connect();
-    try {
-        const queryString = `DROP DATABASE IF EXISTS "${databaseName}"`;
-        await client.query(queryString);
-        console.log(`Database "${databaseName}" dropped successfully.`);
-    } catch (error) {
-        console.error('Error dropping database:', error);
-    } finally {
-        client.release();
-    }
-}
-
-// Create a new table in a specific database
-async function createTable(tableName, columns) {
-    const client = await pool.connect();
-    try {
-        const columnDefs = columns.map(column => `"${column.name}" ${column.type}`).join(', ');
-        const queryString = `CREATE TABLE IF NOT EXISTS "${tableName}" (${columnDefs})`;
-        await client.query(queryString);
-        console.log(`Table "${tableName} created successfully.`);
-    } catch (error) {
-        console.error('Error creating table:', error);
-    } finally {
-        client.release();
-    }
-}
-
-// Update table by adding a column
-async function addColumnToTable(columnName, columnType) {
-    const client = await pool.connect();
-    try {
-        const queryString = `ALTER TABLE practice_table ADD COLUMN "${columnName}" "${columnType}"`;
-        await client.query(queryString);
-        console.log('Column "${columnName}" was added successfully.');
-    } catch (error) {
-        console.error('Error adding column to table:', error);
-    } finally {
-        client.release();
-    }
-}
-
-const columns = [
-    { name: 'jan', type: 'INTEGER' },
-    { name: 'feb', type: 'INTEGER' },
-    { name: 'mar', type: 'INTEGER' },
-    { name: 'apr', type: 'INTEGER' },
-    { name: 'may', type: 'INTEGER' },
-    { name: 'jun', type: 'INTEGER' },
-    { name: 'jul', type: 'INTEGER' },
-    { name: 'aug', type: 'INTEGER' },
-    { name: 'sep', type: 'INTEGER' },
-    { name: 'oct', type: 'INTEGER' },
-    { name: 'nov', type: 'INTEGER' },
-    { name: 'dec', type: 'INTEGER' },
-]
-
-// Utilization of functions
-// createTable('practice_table', columns);
-// addColumnToTable('year', 'NUMERIC')
-// importExcelData('SeriesReport.xlsx', 'BLS Data Series');
-// setupDatabase('practice_db_connection_test');
-// dropDatabase('flights');
 
 
 
